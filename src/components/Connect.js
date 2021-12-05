0e9cd033353e3ed866e03bed98f097a6444e5965
513sojin <513sojin@naver.com>
2021-12-06 02:38:00 +0900

    footer 완성

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -6,35 +6,55 @@ import Fade from "react-reveal/Fade";
 import github from '../image/github.png';
 import velog from '../image/velog.png';
 import { imgContianer } from './Project';
+import Footer from './Footer';
 
 const Connect=()=>{
     return (
         <>
-        <div css={content}>
-        <Fade bottom>
-        <div style={{marginTop: '-10%', marginBottom: '3%'}}>
-            <p css={titleStyle}>Connect</p>
-            <p style={{fontWeight : 600, textAlign: 'center'}}>저의 더 많은 정보가 담긴 곳이 궁금하신가요 ?</p>
-        </div>
-        </Fade>
-        <div css={contentWrapper}>
-            <div css={contentDiv}>
-                <div css={imgContianer}>
-                    <img src={github} width='200' height='200'/>
-                    <p>Github</p>
+        <div css={contentFooter}>
+            <div style={{paddingBottom: '6rem'}}>
+                <Fade bottom>
+                <div style={{ marginBottom: '3%'}}>
+                    <p css={titleStyle}>Connect</p>
+                    <p style={{fontWeight : 600, textAlign: 'center'}}>저의 더 많은 정보가 담긴 곳이 궁금하신가요 ?</p>
                 </div>
-                <div css={imgContianer}>
-                    <img src={velog}/>
-                    <p>Velog</p>
+                </Fade>
+                <div css={connectWrapper}>
+                    <div css={contentDiv}>
+                        <div css={imgContianer}>
+                            <img src={github} width='200' height='200'/>
+                            <p>Github</p>
+                        </div>
+                        <div css={imgContianer}>
+                            <img src={velog}/>
+                            <p>Velog</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            
+            </div>    
+            <Footer/>
         </div>
-
-      </div>
+        
         </>
     );
 }
+
+const contentFooter=css`
+height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+`;
+
+const connectWrapper = css`
+  width: 100%;
+  height: 40%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  `;
 
 export default Connect;
 
---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+/** @jsxImportSource @emotion/react */ 
+import { css } from '@emotion/react'
+
+const Footer =()=>{
+    return (
+        <div css={footer}>
+            <p style={{ fontSize: "14px", color: 'white'}}>
+                Copyright 이소진. all rights reserved.
+                Built with {` `}<a href="https://www.gatsbyjs.org" style={{textDecoration:'none'}}>Gatsby</a>
+                <br />
+                <strong>513sojin@gmail.com</strong>
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
+
+const footer=css`
+padding-top: 1.5%;
+padding-right: 1%;
+text-align: right;
+position: absolute;
+bottom: 0;
+background: gray;
+width: 100%;
+height: 6rem;  
+background: #6495ED;
+        background: -webkit-linear-gradient(20deg, #6495ED, #BA55D3);
+        background:    -moz-linear-gradient(20deg, #6495ED, #BA55D3);
+`;
